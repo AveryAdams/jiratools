12a4d097f4c4b60d529bc7c1de44d04c4bd055c4
ADAMS <x17947@oppd.oppd-ds.com>
2018-12-03 17:58:00 -0600

    Initial commit

---
+++ jiratools/__init__.py
@@ -0,0 +1,4 @@
+
+def poop(thing_to_poop: str = 'turd', qty_to_poop: int = 1):
+    # Testing stuff
+    print("Pooping out {} {}".format(qty_to_poop, thing_to_poop))
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jiratools',
+      version='0.1',
+      description='A collection of tools to work with Jira and other platforms',
+      url='https://github.com/averyadams/jiratools',
+      author='Avery Adams',
+      author_email='avery.adams@gmail.com',
+      license='MIT',
+      packages=['jiratools'],
+      zip_safe=False)
